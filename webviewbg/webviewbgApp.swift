1e048eca6d47b13e25dc95a9a89399e0255696c1
sultanmyrza <sultanmyrza@gmail.com>
2022-11-28 03:08:28 +0800

    Initial Commit

---
+++ webviewbg/webviewbgApp.swift
@@ -0,0 +1,17 @@
+//
+//  webviewbgApp.swift
+//  webviewbg
+//
+//  Created by sultanmyrza on 2022/11/28.
+//
+
+import SwiftUI
+
+@main
+struct webviewbgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
